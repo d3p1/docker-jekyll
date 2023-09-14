5d50806da9bc28e32171c1978fe7c69e001f8e82
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-09-14 02:46:59 +0000

    refactor: init jekyll-serve script to improve container boot [#1]

---
+++ entrypoint/jekyll-serve.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+##
+# @description Script to serve Jekyll
+# @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+# @note        The idea of this script is to run it as the 
+#              Docker entrypoint command. 
+#              It builds Jekyll with a custom configuration
+#              and also executes Nginx startup processes
+# @link https://github.com/nginxinc/docker-nginx/blob/master/entrypoint/docker-entrypoint.sh
+##
+
+##
+# @note Add flag to exit script if there is an error related to a command
+##
+set -e
+
+##
+# Main
+#
+# @param  $1  Script arguments
+# @return int Exit code
+##
+main() {
+    ##
+    # @note Define bootstrap scripts
+    ##
+    local jekyll="/docker-entrypoint.d/jekyll/bootstrap.sh"
+    local nginx="/docker-entrypoint.sh nginx"
+
+    ##
+    # @note Check if scripts have needed permissions to execute them
+    ##
+    echo "Trying to boot Jekyll and Nginx..."
+    if [ -x $jekyll ] && [ -x $nginx ]; then
+        ##
+        # @note Boot Jekyll
+        ##
+        "$jekyll"
+
+        ##
+        # @note Boot Nginx
+        ##
+        "$nginx"
+
+        ##
+        # @note Log success
+        ##
+        echo "Jekyll and Nginx successfully booted ;)"
+        return 0;
+    else
+        ##
+        # @note Log error
+        ##
+        echo "It is not possible to execute $jekyll and $nginx :("
+        return 1;
+    fi 
+}
+
+##
+# @note Execute main function
+##
+main "$@"
